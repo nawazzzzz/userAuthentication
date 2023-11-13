4411caf1e615be9d85408d5016c4e0df75e6b324
Mohd Nawaz Siddiqui <nawazsiddiqui27@gmail.com>
2023-11-13 11:42:24 +0530

    Project Skeleton

---
+++ src/main/java/com/authentication/Authentication/AuthenticationApplication.java
@@ -0,0 +1,15 @@
+package com.authentication.Authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		SpringApplication.run(AuthenticationApplication.class, args);
+	}
+
+}
