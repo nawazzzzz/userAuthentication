0e40fe693e459eb84bce85f3b1ad14f036696929
Mohd Nawaz Siddiqui <nawazsiddiqui27@gmail.com>
2023-11-14 11:27:42 +0530

    Initialized the project with maven build

--- src/main/java/com/authentication/AuthenticationApplication.java
+++ src/main/java/com/authentication/AuthenticationApplication.java
@@ -1,4 +1,4 @@
-package com.authentication.Authentication;
+package com.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
 		SpringApplication.run(AuthenticationApplication.class, args);
 	}
 
